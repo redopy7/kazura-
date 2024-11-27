d28b9aed52de3e0a9d41bcd1072285fcffabe3e7
redopy7 <152308671+redopy7@users.noreply.github.com>
2024-11-28 04:27:29 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('toggle-btn').addEventListener('click', function () {
-    document.body.classList.toggle('light-mode');
-    const isLightMode = document.body.classList.contains('light-mode');
-    this.textContent = isLightMode ? 'Dark Mode' : 'Light Mode';
-});
